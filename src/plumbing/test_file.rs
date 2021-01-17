71de58e483f0e33fada82532562f0cbc3b7c402a
Owen Synge <osynge@googlemail.com>
2021-01-17 23:11:28 +0100

    Use crate::DbConnection

--- src/plumbing/test_file.rs
+++ src/plumbing/test_file.rs
@@ -1,4 +1,5 @@
 use crate::model::test_file::{TestFile, TestFileNew};
+use crate::DbConnection;
 use crate::Pool;
 use actix_web::web;
 use diesel::dsl::insert_into;
@@ -6,16 +7,15 @@ use diesel::prelude::*;
 use diesel::RunQueryDsl;
 
 pub fn add_test_file(
-    pool: web::Data<Pool>,
+    conn: &DbConnection,
     new_directory: &String,
     new_file_name: &String,
 ) -> Result<TestFile, diesel::result::Error> {
     use crate::schema::test_file::dsl::*;
-    let db_connection = pool.get().unwrap();
     match test_file
         .filter(directory.eq(new_directory))
         .filter(file_name.eq(new_file_name))
-        .first::<TestFile>(&db_connection)
+        .first::<TestFile>(conn)
     {
         Ok(p) => return Ok(p),
         Err(_) => {
@@ -25,10 +25,10 @@ pub fn add_test_file(
             };
             insert_into(test_file)
                 .values(&new_test_file_new)
-                .execute(&db_connection)
+                .execute(conn)
                 .expect("Error saving new test_file_new");
 
-            let result = test_file.order(id.desc()).first(&db_connection)?;
+            let result = test_file.order(id.desc()).first(conn)?;
             Ok(result)
         }
     }
