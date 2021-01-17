4479228b0b713b3ae3633e36b4ef980b5ac0bed7
Owen Synge <osynge@googlemail.com>
2021-01-17 21:38:45 +0100

    ichanges to make thigns more testable

---
+++ src/plumbing/enviroment_test.rs
@@ -0,0 +1,24 @@
+use crate::db;
+use crate::model::keyvalue::KeyValueNew;
+use crate::Pool;
+use diesel::dsl::insert_into;
+use diesel::prelude::*;
+use diesel::RunQueryDsl;
+#[test]
+fn create_user_with_phone_and_email() {
+    use crate::schema::keyvalue::dsl::*;
+    /*"PooledConnection<ConnectionManager<SqliteConnection>>"*/
+    let conn = db::establish_connection().get().unwrap();
+    let fred = &conn as &db::DbConnection;
+    let new_key = "test@email.com";
+    let new_value = "123456789";
+
+    let new_keyvalue = KeyValueNew {
+        key: new_key,
+        value: new_value,
+    };
+    insert_into(keyvalue)
+        .values(&new_keyvalue)
+        .execute(fred)
+        .expect("Error saving new keyvalue");
+}
