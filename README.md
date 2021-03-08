e3db3b3f1931fce66ecd393d71f2f81a74d34487
Owen Synge <osynge@googlemail.com>
2021-03-08 23:32:48 +0100

    Now we support creating the database and provide nice errors

--- README.md
+++ README.md
@@ -1,31 +1,12 @@
+# Diesel database for [xunit-repo](https://github.com/osynge/xunit-repo).
 
-migrate db:
+## To migrate db:
 
+```
 rm repo.db src/schema.rs
 diesel migration run
+```
 
+## TODO
 
-Test commands:
-
-
-curl -v -X POST -d '{ "sk": "mykey", "identiifier": "identiifier2", "human_name" : "human_name" }' -H 'Content-Type: application/json' http://127.0.0.1:8888/project_add
-curl -v -X POST -d '{  "sk": "f8f1208d-bf03-4daf-b919-ab18c20138b0" }' -H 'Content-Type: application/json' http://127.0.0.1:8888/project_add
-
-
-curl -v -X POST -d '{  "key": "HOME", "value" : "/home/username" }' -H 'Content-Type: application/json' http://127.0.0.1:8888/keyvalue_add
-
-
-
-curl -v -X POST -d '{  "key_value": [{  "key": "HOME", "value" : "/home/username" }] }' -H 'Content-Type: application/json' http://127.0.0.1:8888/enviroment_add
-
-
-curl -v -X POST -d '{ "client_identifier" : "1"  }' -H 'Content-Type: application/json' http://127.0.0.1:8888/run_add
-
-curl -v -X POST -d '{ "name" : "name" , "classname" : "classname", "time" : 1, "error_type" : "error_type", "error_message" : "error_message", "error_description" : "error_description", "system_out": "system_out", "system_err" :"system_err" }' -H 'Content-Type: application/json' http://127.0.0.1:8888/test_case_error_add
-
-
-curl -v -X POST -d '{ "name" : "name" , "classname" : "classname", "time" : 1, "failure_type" : "edddddrror_type", "failure_message" : "failure_message", "failure_description" : "failure_description", "system_out": "system_out", "system_err" :"system_err" }' -H 'Content-Type: application/json' http://127.0.0.1:8888/test_case_failure_add
-
-curl -v -X POST -d '{ "name" : "name" , "classname" : "classname", "time" : 1, "skipped_message" : "edddddrror_type" }' -H 'Content-Type: application/json' http://127.0.0.1:8888/test_case_skipped_add
-
-curl -v -X POST -d '{ "name" : "namedddasas" , "classname" : "classnameddddassas", "time" : 1  }' -H 'Content-Type: application/json' http://127.0.0.1:8888/test_case_pass_add
+* Using cargo features add support for pgsql.
--- src/db.rs
+++ src/db.rs
@@ -3,24 +3,74 @@ use crate::Pool;
 use diesel::prelude::*;
 use diesel::r2d2::ConnectionManager;
 use diesel::SqliteConnection;
-use url::Url;
+use thiserror::Error;
+use url::{Host, Url};
 
 embed_migrations!();
 pub fn run_migrations(conn: &SqliteConnection) {
     embedded_migrations::run(conn);
 }
 
-pub fn sql_lite_establish_connection_mem() -> Pool {
+#[derive(Error, Debug)]
+pub enum ConnectionError {
+    #[error("url parsing error")]
+    UrlParseError(#[from] url::ParseError),
+    #[error("io parsing error")]
+    IoErr(#[from] std::io::Error),
+    #[error("File not found '{0}'.")]
+    FilePathIsNotFile(String),
+    #[error("Db connection scheme is invalid '{0}'.")]
+    InvalidConnectionScheme(String),
+    #[error("Db connection host is invalid '{0}'.")]
+    InvalidConnectionHost(String),
+}
+
+#[derive(Debug, PartialEq)]
+enum DbConnectParams {
+    SqlLiteMemory,
+    SqlLitePath(String),
+}
+
+fn parse_connection_url_sqlite(connection_url: &Url) -> Result<DbConnectParams, ConnectionError> {
+    match connection_url.host() {
+        Some(p) => match p {
+            Host::Domain(d) => match d {
+                "memory" => Ok(DbConnectParams::SqlLiteMemory),
+                _ => Err(ConnectionError::InvalidConnectionHost(d.to_string())),
+            },
+            Host::Ipv4(a) => Err(ConnectionError::InvalidConnectionHost(a.to_string())),
+            Host::Ipv6(a) => Err(ConnectionError::InvalidConnectionHost(a.to_string())),
+        },
+        None => Ok(DbConnectParams::SqlLitePath(
+            connection_url.path().to_string(),
+        )),
+    }
+}
+
+fn parse_connection_url(url: &str) -> Result<DbConnectParams, ConnectionError> {
+    let parsed_url = Url::parse(url)?;
+    match parsed_url.scheme() {
+        "sqlite" => parse_connection_url_sqlite(&parsed_url),
+        _ => Err(ConnectionError::InvalidConnectionScheme(
+            parsed_url.scheme().to_string(),
+        )),
+    }
+}
+
+pub fn sql_lite_establish_connection_mem() -> Result<Pool, ConnectionError> {
     let manager = ConnectionManager::<SqliteConnection>::new(":memory:");
     let database_pool = Pool::builder()
         .build(manager)
         .expect("Failed to create DB pool.");
 
     run_migrations(&database_pool.get().unwrap());
-    database_pool
+    Ok(database_pool)
 }
 
-pub fn sql_lite_establish_connection_file(database_path: &str, create_db: bool) -> Pool {
+pub fn sql_lite_establish_connection_file(
+    database_path: &str,
+    create_db: bool,
+) -> Result<Pool, ConnectionError> {
     if create_db {
         let connection = SqliteConnection::establish(&database_path).unwrap();
         run_migrations(&connection);
@@ -29,15 +79,13 @@ pub fn sql_lite_establish_connection_file(database_path: &str, create_db: bool) 
         .build(ConnectionManager::<SqliteConnection>::new(database_path))
         .expect("Failed to create DB pool.");
     run_migrations(&database_pool.get().unwrap());
-    database_pool
+    Ok(database_pool)
 }
-pub fn establish_connection_pool(input: &str, create_db: bool) -> Pool {
-    let dburl = Url::parse(input).unwrap();
-    match dburl.scheme() {
-        "memory" => sql_lite_establish_connection_mem(),
-        "sqlite" => sql_lite_establish_connection_file(dburl.path(), create_db),
-        _ => {
-            panic!("url scheme is invalid.");
+pub fn establish_connection_pool(input: &str, create_db: bool) -> Result<Pool, ConnectionError> {
+    match parse_connection_url(input)? {
+        DbConnectParams::SqlLiteMemory => Ok(sql_lite_establish_connection_mem()?),
+        DbConnectParams::SqlLitePath(path) => {
+            Ok(sql_lite_establish_connection_file(&path, create_db)?)
         }
     }
 }
@@ -46,8 +94,31 @@ mod tests {
     use super::*;
     use tempdir;
     #[test]
+    fn parse_url_sqlite_mem() {
+        assert!(parse_connection_url("sqlite://memory").unwrap() == DbConnectParams::SqlLiteMemory)
+    }
+    #[test]
+    fn parse_url_sqlite_relative_file() {
+        assert!(
+            parse_connection_url("sqlite:relative.db").unwrap()
+                == DbConnectParams::SqlLitePath("relative.db".to_string())
+        )
+    }
+
+    #[test]
+    fn parse_url_sqlite_absolute_file() {
+        assert!(
+            parse_connection_url("sqlite:/tmp/absolute.db").unwrap()
+                == DbConnectParams::SqlLitePath("/tmp/absolute.db".to_string())
+        )
+    }
+
+    #[test]
     fn establish_connection_in_mem() {
-        establish_connection_pool("memory://", true).get().unwrap();
+        let foo = establish_connection_pool("sqlite://memory", true)
+            .unwrap()
+            .get()
+            .unwrap();
     }
     #[test]
     fn establish_connection_on_disk() {
@@ -55,7 +126,10 @@ mod tests {
             tempdir::TempDir::new("establish_connection_on_disk").expect("create temp dir");
         let file_path = tmp_dir.path().join("sqlite.db");
         let url = format!("sqlite://{}", file_path.to_str().unwrap());
-        let foo = establish_connection_pool(&url, true).get().unwrap();
+        let foo = establish_connection_pool(&url, true)
+            .unwrap()
+            .get()
+            .unwrap();
         foo.begin_test_transaction();
     }
 }
